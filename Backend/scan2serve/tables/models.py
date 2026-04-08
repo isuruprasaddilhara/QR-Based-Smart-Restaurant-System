d1ef72b6ac548323e14ed055ba3d97d390309424
Isuru Prasad <41295242+isuruprasaddilhara@users.noreply.github.com>
2026-04-08 09:46:02 +0530

    update models.py

--- Backend/scan2serve/tables/models.py
+++ Backend/scan2serve/tables/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Table(models.Model):
     table_number = models.PositiveIntegerField(unique=True, null=True, blank=True)
     qr_code = models.CharField(max_length=255, unique=True)
-    status = models.BooleanField(default=False)
+    status = models.BooleanField(default=False) #true = occupied, false = available
     section = models.CharField(max_length=100, blank=True, null=True)
     capacity = models.PositiveIntegerField(default=2)
 
